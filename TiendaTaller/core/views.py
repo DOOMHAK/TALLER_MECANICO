22a35467b6cbab85e48e93e13de50d1e662e340b
DOOMHAK <ra.nunezp@duocuc.cl>
2023-06-30 14:04:37 -0400

    avance 2-04

--- TiendaTaller/core/views.py
+++ TiendaTaller/core/views.py
@@ -76,14 +76,20 @@ def vehiculo(request, action, id):
 
 def poblar_bd(request):
     Vehiculo.objects.all().delete()
-    Vehiculo.objects.create(patente="ALAN67", marca='Volvo', modelo="Volvo Station Wagon", imagen="images/volvosw.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=1))
-    Vehiculo.objects.create(patente="BILL88", marca='Saleen', modelo="S7", imagen="images/saleen.jpg", categoria=Categoria.objects.get(idCategoria=1))
-    Vehiculo.objects.create(patente="ELVI54", marca='Shelby', modelo="Cobra de 1967", imagen="images/cobra.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=2))
-    Vehiculo.objects.create(patente="FEDE84", marca='Mercedes-Benz', modelo="Pagoda de 1972", imagen="images/pagoda.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=2))
-    Vehiculo.objects.create(patente="JEFF46", marca='Ford', modelo="Wolf WR1 Ford Race Car", imagen="images/wolf.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=1))
-    Vehiculo.objects.create(patente="JOHN80", marca='Ford', modelo="Flathead Roadster de 1932", imagen="images/flathead.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=2))
-    Vehiculo.objects.create(patente="PAUL62", marca='Rolls-Royce', modelo="Phantom", imagen="images/phantom.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=1))
-    Vehiculo.objects.create(patente="SCAR47", marca='Mustang', modelo="Mustang de 1970", imagen="images/mustang.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=2))
-    Vehiculo.objects.create(patente="TIRO98", marca='Mercedes-Benz', modelo="Iron Bike de 1998", imagen="images/motoiron.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=3))
-    Vehiculo.objects.create(patente="UVAM20", marca='Silver Plus', modelo="Silver de 2000", imagen="images/silver.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=3))
+    Vehiculo.objects.create(patente="ALAN67", marca='Volvo', modelo="Volvo Station Wagon", imagen="images/auto1.jpg", servicio="Suspension y Direccion", detalle_servicio="arreglo frenos", categoria=Categoria.objects.get(idCategoria=1))
+    Vehiculo.objects.create(patente="BILL88", marca='Saleen', modelo="S7", imagen="images/auto2.jpg", servicio="electronica automotriz", detalle_servicio="cambio focos traseros",  categoria=Categoria.objects.get(idCategoria=1))
+    Vehiculo.objects.create(patente="JEFF46", marca='Ford', modelo="Wolf WR1 Ford Race Car", imagen="images/auto3.jpg", servicio="cajas de cambio", detalle_servicio="relaciones de 6 a 9", categoria=Categoria.objects.get(idCategoria=1))
+    Vehiculo.objects.create(patente="PAUL62", marca='Rolls-Royce', modelo="Phantom", imagen="images/auto4.jpg", servicio="Suspension y Direccion", detalle_servicio="arreglo direcciones laterales", categoria=Categoria.objects.get(idCategoria=1))
+
+    Vehiculo.objects.create(patente="ELVI54", marca='Shelby', modelo="Cobra de 1967", imagen="images/auto5.jpg",servicio="Suspension y Direccion",detalle_servicio="ajustes de suspension trasero", categoria=Categoria.objects.get(idCategoria=2))
+    Vehiculo.objects.create(patente="FEDE84", marca='Mercedes-Benz', modelo="Pagoda de 1972", imagen="images/auto6.jpg",servicio="cajas de cambio",detalle_servicio="relaciones de 8 a 5", categoria=Categoria.objects.get(idCategoria=2))
+    Vehiculo.objects.create(patente="JOHN80", marca='Ford', modelo="Flathead Roadster de 1932", imagen="images/auto7.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo completo suspension y direccion", categoria=Categoria.objects.get(idCategoria=2))
+    Vehiculo.objects.create(patente="SCAR47", marca='Mustang', modelo="Mustang de 1970", imagen="images/auto8.jpg",servicio="electronica automotriz",detalle_servicio="cambio de cables traseros", categoria=Categoria.objects.get(idCategoria=2))
+
+    Vehiculo.objects.create(patente="TIRO98", marca='Mercedes-Benz', modelo="Iron Bike de 1998", imagen="images/auto9.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo rueda frontal de direccion", categoria=Categoria.objects.get(idCategoria=3))
+    Vehiculo.objects.create(patente="UVAM20", marca='Silver Plus', modelo="Silver de 2000", imagen="images/auto10.jpg",servicio="electronica automotriz",detalle_servicio="arreglo completo de focos", categoria=Categoria.objects.get(idCategoria=3))
     return redirect(vehiculo, action='ins', id = '-1')
+
+
+
+
