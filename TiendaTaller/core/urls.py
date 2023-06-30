b5e0bfc53ef40ad46d63eee917c71a728f7d67af
DOOMHAK <ra.nunezp@duocuc.cl>
2023-06-30 12:27:55 -0400

    avance pagina

--- TiendaTaller/core/urls.py
+++ TiendaTaller/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, poblar_bd, vehiculo, vehiculo_tienda, vehiculo_ficha
+from .views import home, poblar_bd, vehiculo, vehiculo_tienda, vehiculo_ficha,nosotros,pag_caja,pag_electri,pag_suspe,contacto,registro,iniciosesion
 
 urlpatterns = [
     path('', home, name="home"),
@@ -7,4 +7,12 @@ urlpatterns = [
     path('vehiculo/<action>/<id>', vehiculo, name="vehiculo"),
     path('vehiculo_tienda', vehiculo_tienda, name="vehiculo_tienda"),
     path('vehiculo_ficha/<id>', vehiculo_ficha, name="vehiculo_ficha"),
+    path('contacto' ,contacto, name="contacto"),
+    path('iniciosesion' ,iniciosesion, name= "iniciosesion"),
+    path('nosotros',nosotros, name="nosotros"),
+    path('pag_caja', pag_caja, name="pag_caja"),
+    path('pag_electri', pag_electri, name="pag_electri"),
+    path('pag_suspe', pag_suspe, name="pag_suspe"),
+    path('registro', registro, name="registro"),
+
 ]
