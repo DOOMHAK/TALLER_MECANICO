8c97375de7bf39f16dcbddac7f7eaa01f1061426
Masterk2123 <ke.arucutipa@duocuc.cl>
2023-06-30 19:59:27 -0400

    api_02

---
+++ TiendaTaller/core/urls.py
@@ -0,0 +1,16 @@
+from rest_framework import routers
+from django.urls import path
+from .views import home, poblar_bd, vehiculo, vehiculo_tienda, vehiculo_ficha
+from .api import ProjectViewSet
+
+router = routers.DefaultRouter()
+
+router.register('api/projects', ProjectViewSet, 'projects')
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('poblar_bd', poblar_bd, name="poblar_bd"),
+    path('vehiculo/<action>/<id>', vehiculo, name="vehiculo"),
+    path('vehiculo_tienda', vehiculo_tienda, name="vehiculo_tienda"),
+    path('vehiculo_ficha/<id>', vehiculo_ficha, name="vehiculo_ficha"),
+]
